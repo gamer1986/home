64b0055b53a7b1cfe3336805b365f35c0236cbb8
gamer1986 <nikolaievshel@gmail.com>
2014-08-24 03:25:58 +0600

    2

---
+++ shop1/www/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.shop/admin/');
+define('HTTP_CATALOG', 'http://www.shop/');
+define('HTTP_IMAGE', 'http://www.shop/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.shop/admin/');
+define('HTTPS_CATALOG', 'http://www.shop/');
+define('HTTPS_IMAGE', 'http://www.shop/image/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\shop\www/admin/');
+define('DIR_SYSTEM', 'Z:\home\shop\www/system/');
+define('DIR_DATABASE', 'Z:\home\shop\www/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\shop\www/admin/language/');
+define('DIR_TEMPLATE', 'Z:\home\shop\www/admin/view/template/');
+define('DIR_CONFIG', 'Z:\home\shop\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\shop\www/image/');
+define('DIR_CACHE', 'Z:\home\shop\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\shop\www/download/');
+define('DIR_LOGS', 'Z:\home\shop\www/system/logs/');
+define('DIR_CATALOG', 'Z:\home\shop\www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cms_30');
+define('DB_PREFIX', '');
+?>
---
+++ shop1/www/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.shop/');
+define('HTTP_IMAGE', 'http://www.shop/image/');
+define('HTTP_ADMIN', 'http://www.shop/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.shop/');
+define('HTTPS_IMAGE', 'http://www.shop/image/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:\home\shop\www/catalog/');
+define('DIR_SYSTEM', 'Z:\home\shop\www/system/');
+define('DIR_DATABASE', 'Z:\home\shop\www/system/database/');
+define('DIR_LANGUAGE', 'Z:\home\shop\www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:\home\shop\www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:\home\shop\www/system/config/');
+define('DIR_IMAGE', 'Z:\home\shop\www/image/');
+define('DIR_CACHE', 'Z:\home\shop\www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:\home\shop\www/download/');
+define('DIR_LOGS', 'Z:\home\shop\www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cms_30');
+define('DB_PREFIX', '');
+?>
